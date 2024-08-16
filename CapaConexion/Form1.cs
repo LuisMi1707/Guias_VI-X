68736e11598038490d1340f3d7dbfb18a63a9909
LuisMi1707 <126534976+LuisMi1707@users.noreply.github.com>
2024-08-15 21:41:11 -0600

    commit de cambio guia 8

--- CapaConexion/Form1.cs
+++ CapaConexion/Form1.cs
@@ -24,12 +24,44 @@ namespace CapaConexion
                 new SqlConnection("Data Source=LAPTOP-2JMS5E5I\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
             MessageBox.Show("Conexion creada");
             conexion.Open();
-            string selectFrom = "SELECT * FROM [dbo].[Customers]";
+
+
+
+            //------------------------
+
+            String selectFrom = "";
+            selectFrom = selectFrom + "SELECT [CompanyName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
+            selectFrom = selectFrom + "      ,[Address] " + "\n";
+            selectFrom = selectFrom + "      ,[City] " + "\n";
+            selectFrom = selectFrom + "      ,[Region] " + "\n";
+            selectFrom = selectFrom + "      ,[PostalCode] " + "\n";
+            selectFrom = selectFrom + "      ,[Country] " + "\n";
+            selectFrom = selectFrom + "      ,[Phone] " + "\n";
+            selectFrom = selectFrom + "      ,[Fax] " + "\n";
+            selectFrom = selectFrom + "  FROM [dbo].[Customers]";
+
+            //-----------------------
+
+
+
+
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
-            conexion.Close();
+            while (reader.Read())
+            {
+                var customerId = reader["CompanyName"];
+            }
+
             MessageBox.Show("Conexion cerrada");
+            conexion.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
