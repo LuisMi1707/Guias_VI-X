6ea85c08276b300afa7bb8818792a378bfe6b19c
LuisMi1707 <126534976+LuisMi1707@users.noreply.github.com>
2024-08-15 22:45:37 -0600

    commit guia IX

--- CapaConexion/Form1.cs
+++ CapaConexion/Form1.cs
@@ -50,9 +50,24 @@ namespace CapaConexion
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
+            List<Customers> Customers = new List<Customers>();
+
             while (reader.Read())
             {
-                var customerId = reader["CompanyName"];
+
+                Customers customers = new Customers();
+                customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
+                customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
+                customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
+                customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
+                customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
+                customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
+                customers.PostalCode = reader["PostalCode"] == DBNull.Value ? 0 : (int)reader["PostalCode"];
+                customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
+                customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
+                customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+
+                Customers.Add(customers);
             }
 
             MessageBox.Show("Conexion cerrada");
